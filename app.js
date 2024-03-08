ac0c93489628de2adb5f7649d40f59785eaf04d3
andrejuvland <andre_juvland@hotmail.com>
2024-03-08 14:12:55 +0100

    First commit

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const {buildSchema} = require('graphql');
+require("dotenv").config();
+const db = require("./models");
+db.sequelize.sync({ force: false });
 
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+// Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-type Query {
-    hello: String
-}
-`);
-
-const root = {
-    hello: () => {
-        return 'Hello World!';
+    type Query {
+      hello: String
     }
+  `);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return "Hello world!";
+  },
 };
 
-const app = express();
-app.use('/graphql', graphqlHTTP({
+var app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     rootValue: root,
-    graphql: true,
-}));
+    graphiql: true,
+  })
+);
 app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
